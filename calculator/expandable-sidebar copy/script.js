357b05621c0a7b0674a66462c07d7246ce96f2d9
saitejx <saitejatelukutla@gmail.com>
2026-06-01 15:37:57 +0530

    added expense tracker

--- calculator/expandable-sidebar copy/script.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.querySelector("#sidebar");
-const toggleBtn = document.querySelector("#toggleBtn");
-
-toggleBtn.addEventListener("click", () => {
-  const isCollapsed = sidebar.classList.toggle("collapsed");
-  toggleBtn.querySelector(".toggle-text").textContent = isCollapsed
-    ? "Expand"
-    : "Collapse";
-});
